688db4285796bd57268f6dabf23bfc71d221a016
Matt Rose <rose.matt@gmail.com>
2018-02-04 12:24:07 -0500

    Add basic application index

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Assuming you've installed PlaceHolder via composer, i.e.
+ * `composer require seydoggy/placeholder`
+ * Then you should be able to access your classes by requiring the composer
+ * autoloader file.
+ */
+require __DIR__ . '/vendor/autoload.php';
+/**
+ * Define a variable or constant that points to the image folder
+ */
+define('IMAGEPATH', dirname(__FILE__).'/images/');
+/**
+ * Initialize the PlaceHolder object
+ */
+$placeholder = new \seydoggy\PlaceHolder(IMAGEPATH);
